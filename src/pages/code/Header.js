21a11e0ba29d03f69c67e7920994940c9c606ae5
Janos <jfazakas@gmail.com>
2022-02-12 20:53:29 +0200

    updated code profile

--- src/pages/code/Header.js
+++ src/pages/code/Header.js
@@ -9,23 +9,64 @@ const Header = () => {
         technology solutions, and Agile project delivery experience.
       </p>
       <p className="indent">
-        Recognized for demonstrating a natural aptitude for optimizing business
-        operations by developing impactful software solutions, as well as for
-        modernizing legacy applications and ensuring on-time project delivery I
-        have a verifiable history of contributing directly to company success
-        throughout my career. As such, I have consistently exceeded project
-        goals and I am adept at achieving maximum operational output with
-        minimal resource expenditure.
+        My programming experience started with{" "}
+        <a href="http://en.wikipedia.org/wiki/BASIC" target="_blank">
+          BASIC
+        </a>{" "}
+        back in the days, on a{" "}
+        <a href="http://en.wikipedia.org/wiki/ZX_Spectrum" target="_blank">
+          ZX Spectrum
+        </a>{" "}
+        home computer. Now that was really fun, using an actual black and white
+        TV and a tape-player/recorder to load and save "software".
       </p>
       <p className="indent">
-        Professional focal points include Agile software development, web
-        development, full-stack development, team collaboration, requirements
-        gathering, technology implementation, strategic planning, issue
-        resolution, and process optimization. Delivering superior administration
-        on the latter areas of expertise requires utilization of effective
-        communication skills, analytical skills, as well as professional acumen,
-        change management, project management, and resource management to
-        support efficiency and maximum returns.
+        The next step was microcontroller programming for a few applications
+        with PIC and ATMEL controllers. At this point I really enjoyed
+        programming and I was looking forward to learn some higher level
+        languages.
+      </p>
+      <p className="indent">
+        During my high school years I've got introduced to{" "}
+        <a href="http://en.wikipedia.org/wiki/ANSI_C" target="_blank">
+          ANSI C
+        </a>{" "}
+        programming, which turned out to be quite a different approach and
+        somehow changed the definition of programming. This was followed by
+        <a
+          href="http://en.wikipedia.org/wiki/Object-oriented_programming"
+          target="_blank"
+        >
+          {" "}
+          Object Oriented Programming
+        </a>
+        , C++ with all the new - sometimes twisted - approaches and at this
+        point it was clear to me, that I am walking a path without a target,
+        that programming is a lifelong learning process which demands an
+        all-time open mindedness and willingness to make the step which doesn't
+        necessarily gets you closer to any target but definitely takes you out
+        from your last comfort zone.
+      </p>
+      <p className="indent">
+        While at university I learnd about{" "}
+        <a href="http://en.wikipedia.org/wiki/Unix-like" target="_blank">
+          *nix
+        </a>{" "}
+        systems, databases and different scripting and mark-up languages. Cool
+        stuff. I became a big fan of Open Source Software and also I have a
+        great respect for people who develop code for the sake of creating some
+        useful tools for the humanity, without any commercial considerations.
+        Guys, you might never read this, but still, thumbs up!
+      </p>
+      <p className="indent">
+        Later on, the{" "}
+        <a href="http://en.wikipedia.org/wiki/World_Wide_Web" target="_blank">
+          World Wide Web
+        </a>{" "}
+        started to develop really fast, opening new fascinating opportunities
+        for the programmers and myself of course. I got from writing code
+        through programming machines and devices to do stuff, to actual software
+        development and right now enjoying WEB application development.
       </p>
     </div>
   );
