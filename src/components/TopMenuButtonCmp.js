166a15ddba15e9d491042dd1dfcef76d83a8abf9
Janos <jfazakas@gmail.com>
2022-02-12 15:07:47 +0200

    added navigate home to logo

--- src/components/TopMenuButtonCmp.js
+++ src/components/TopMenuButtonCmp.js
@@ -1,14 +1,21 @@
 import IconButton from "@material-ui/core/IconButton";
+import { useNavigate } from "react-router-dom";
 import SettingsInputAntennaIcon from "@mui/icons-material/SettingsInputAntenna";
 // import MenuIcon from "@mui/icons-material/Menu";
 
 const TopMenuButtonCmp = ({ classes }) => {
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    navigate("/");
+  };
+
   return (
     <IconButton
       edge="start"
       className={classes.menuButton}
       color="inherit"
       aria-label="menu"
+      onClick={onClickHandler}
     >
       {/* <SettingsInputAntennaIcon /> */}
       YO6GZI
